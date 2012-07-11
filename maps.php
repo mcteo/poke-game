fa9ddfc2ef2394402b515a20076323ad646e2cf9
mc_teo <mc_teo@live.ie>
2012-07-11 22:02:09 +0100

    Fixed maps.php

--- maps.php
+++ maps.php
@@ -33,19 +33,38 @@
         $map_name = (string)$_POST["map_name"];
 
         $obj = array(
-            
-            "" => "",
-        )
+            "mapID" => $map_name,
+            "imagePath" => $map_path,
+            "noWalkArea" => array(),
+            "eventList" => array(),
+        );
 
-        $cursor = $collection->find(array("mapID" => $map_name));
+        $cursor = $collection->insert($obj);
+
+        echo json_encode("insert", $obj);
     }
 
 
     /*
         Here I define my "schema",
 
-        mapID: 
+        mapID: (string) a unique identifer for map
+        imagePath: (string) relative path to the image file
+        noWalkArea: (Array of TRect) list of rectangles thare are not walkable
+        eventList: (Array of TEvent) list of anything that triggers interaction
+    
+        A TRect being 4 float array (e.g. x1, y1, x2, y2)
+        A TEvent is an basically an associative array/object:
+            type: (string) sign, border, portal, etc (cant think of others)
+            position: (TRect) the events position
+            data: (TVarient) any addition info, (differs by type)
+                    so like a sign would have contents of sign here
+                    and a portal would have mapID of linking portal here
 
+
+        border is a passive threshold, like  between routes,
+        portal is like a door, requiring interation
+        (should i do it this way, or leave everything like portals)
 
     */
 ?>
